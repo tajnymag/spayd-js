5ca0b6217acef4959b9312162ba25d840163a273
Marek Lukáš <tajnymag@gmail.com>
2024-01-17 11:59:19 +0100

    Added a test case for xvs starting with a zero

--- test/spayd.ts
+++ test/spayd.ts
@@ -30,6 +30,15 @@ describe('spayd()', () => {
 		})).toBe('SPD*1.0*ACC:CZ5855000000001265098001+RZBCCZPP*ALT-ACC:CZ5855000000001265098001+RZBCCZPP,CZ5855000000001265098001*AM:480.55*CC:CZK*CRC32:B51BE0B4*DT:20180420*MSG:PAYMENT FOR SOME STUFF*PT:P2P*RF:1234567890123456*RN:PETR DVORAK*X-ID:1234567890*X-KS:1234567890*X-PER:7*X-VS:1234567890');
 	});
 
+	it('allows xvs starting with a zero', () => {
+		expect(spayd({
+			acc: 'CZ2806000000000168540115',
+			am: '450.00',
+			cc: 'CZK',
+			xvs: '0123456789'
+		})).toBe('SPD*1.0*ACC:CZ2806000000000168540115*AM:450.00*CC:CZK*X-VS:0123456789');
+	});
+
 	it('throws an exception when the amount is separated by comma', () => {
 		expect(() => spayd({
 			acc: 'CZ2806000000000168540115',
